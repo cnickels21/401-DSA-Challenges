a9a96494af88fff6434b284f6f66084749efad3d
cnickels21 <cnickels_21@yahoo.com>
2020-05-04 14:56:28 -0500

    third example added

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -10,12 +10,16 @@ namespace Challenges
 
             int[] firstArray = new int[4] { 1, 2, 3, 4 };
             int[] secondArray = new int[5] { 5, 6, 7, 8, 9 };
+            int[] thirdArray = new int[6] { 5, -5, 12, 14, -22, 2 };
 
             Console.WriteLine("This is the first input array: " + string.Join(",", firstArray));
             ReverseArray(firstArray);
 
             Console.WriteLine("This is the second input array: " + string.Join(",", secondArray));
             ReverseArray(secondArray);
+
+            Console.WriteLine("This is the second input array: " + string.Join(",", thirdArray));
+            ReverseArray(thirdArray);
 
             Console.ReadLine();
         }
