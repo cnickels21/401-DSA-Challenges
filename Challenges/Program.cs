01679b12201d4167f075a06933f79f11f31b7e09
cnickels21 <cnickels_21@yahoo.com>
2020-05-04 14:54:54 -0500

    reverse array working with ints

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -5,9 +5,32 @@ namespace Challenges
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Hello World!");
+
+            int[] firstArray = new int[4] { 1, 2, 3, 4 };
+            int[] secondArray = new int[5] { 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("This is the first input array: " + string.Join(",", firstArray));
+            ReverseArray(firstArray);
+
+            Console.WriteLine("This is the second input array: " + string.Join(",", secondArray));
+            ReverseArray(secondArray);
+
             Console.ReadLine();
         }
+
+        public static void ReverseArray(int[] array)
+        {
+            int[] reversedArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                reversedArray[i] = array[array.Length - (1 + i)];
+            }
+
+            Console.WriteLine("Output: " + string.Join(",", reversedArray));
+        }
+
     }
 }
