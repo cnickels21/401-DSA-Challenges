e01e9605269c0d595c31ae0d30a5204375f7254a
cnickels21 <cnickels_21@yahoo.com>
2020-05-05 17:13:37 -0500

    test built out but failed first time

---
+++ Challenges.Tests/ArrayShiftTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Challenges.Tests
+{
+    public class ArrayShiftTests
+    {
+        [Fact]
+        public void Can_insert_number_into_middle_of_array()
+        {
+            // Arrange
+            int[] inputArray = { 1, 2, 3, 4 };
+            int inputNumber = 5;
+            int[] expected = { 1, 2, 5, 3, 4 };
+
+            // Act
+            int[] result = ArrayChallenges.InsertShiftArray(inputArray, inputNumber);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
--- Challenges/ArrayChallenges.cs
+++ Challenges/ArrayChallenges.cs
@@ -6,6 +6,7 @@ namespace Challenges
 {
     public class ArrayChallenges
     {
+        // First Array Challenge Method
         public static int[] ReverseArray(int[] array)
         {
             int[] reversedArray = new int[array.Length];
@@ -17,5 +18,38 @@ namespace Challenges
 
             return reversedArray;
         }
+
+        // Second Array Challenge Method
+        public static int[] InsertShiftArray(int[] array, int number)
+        {
+            // Variables to get middle index for insertion
+            int middleIndex = 0;
+            decimal valueIfArrayLengthIsOdd = array.Length / 2;
+
+            // Returning value of index for even or odd array lengths
+            if (array.Length % 2 == 0)
+            {
+                middleIndex = array.Length / 2;
+            }
+            else if (array.Length % 2 != 0)
+            {
+                decimal roundedIndex = Math.Ceiling(valueIfArrayLengthIsOdd);
+                middleIndex = (int)roundedIndex;
+            }
+            
+            // New array is created below
+            int[] shiftedArray = new int[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                shiftedArray[i] = array[i];
+            }
+
+            shiftedArray[middleIndex] = number;
+
+            return shiftedArray;
+            
+        }
+
     }
 }
