79fd62b74596cb1cb953a79fc7b9a5d4365108c2
cnickels21 <cnickels_21@yahoo.com>
2020-05-06 23:06:17 -0500

    tests passed for given examples

---
+++ Challenges.Tests/binarySearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Challenges.Tests
+{
+    public class binarySearch
+    {
+        [Fact]
+        public void Finds_index_location()
+        {
+            int[] input = new[] { 4, 8, 15, 16, 23, 42 };
+            int result = ArrayChallenges.BinarySearch(input, 15);
+            Assert.Equal(2, result);
+        }
+        [Fact]
+        public void Number_does_not_exist()
+        {
+            int[] input = new[] { 11, 22, 33, 44, 55, 66, 77 };
+            int result = ArrayChallenges.BinarySearch(input, 90);
+            Assert.Equal(-1, result);
+        }
+    }
+}
--- Challenges/ArrayChallenges.cs
+++ Challenges/ArrayChallenges.cs
@@ -61,5 +61,32 @@ namespace Challenges
             
         }
 
+
+        // Binary search method
+
+        public static int BinarySearch(int[] array, int insertedValue)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            int notFound = -1;
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+                if (insertedValue == array[mid])
+                {
+                    return mid;
+                }
+                else if (insertedValue < array[mid])
+                {
+                    high = mid - 1;
+                }
+                else if (insertedValue > array[mid])
+                {
+                    low = mid + 1;
+                }
+            }
+            return notFound;
+        }
+
     }
 }
