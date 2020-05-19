dfa4f06e87e08494cd4c5dfae91b622995172402
cnickels21 <cnickels_21@yahoo.com>
2020-05-18 20:26:18 -0500

    can enqueue single item into list

--- Challenges.Tests/StacksAndQueues.Tests/PseudoqueueTests.cs
+++ Challenges.Tests/StacksAndQueues.Tests/PseudoqueueTests.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using Challenges.StacksAndQueues;
+using DataStructures.Stack;
+using Xunit;
 
 namespace Challenges.Tests.StacksAndQueues.Tests
 {
-    class PseudoqueueTests
+    public class PseudoqueueTests
     {
+
+        [Fact]
+        public void Can_enqueue_into_empty_queue()
+        {
+            // Arrange
+            Pseudoqueue<int> testQueue = new Pseudoqueue<int>();
+
+            // Act
+            testQueue.Enqueue(1);
+
+            // Assert
+            Assert.Equal(1, testQueue.Peek());
+        }
     }
 }
--- Challenges/StacksAndQueues/Pseudoqueue.cs
+++ Challenges/StacksAndQueues/Pseudoqueue.cs
@@ -1,10 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using DataStructures.Stack;
 
 namespace Challenges.StacksAndQueues
 {
-    class Pseudoqueue
+    public class Pseudoqueue<T>
     {
+        // Stacks instantiated for pseudoqueue behavior
+        Stack<T> firstStack = new Stack<T>();
+        Stack<T> secondStack = new Stack<T>();
+
+        // Pseudoqueue field sets defined
+        private Node front;
+        private Node rear;
+
+
+        public void Enqueue(T value)
+        {
+            while (!firstStack.IsEmpty())
+            {
+                secondStack.Push(firstStack.Pop());
+            }
+
+            Node itemToInsert = new Node(firstStack.Push(value));
+
+            if (front == null)
+            {
+                front = rear = itemToInsert;
+            }
+
+            while (!secondStack.IsEmpty())
+            {
+                firstStack.Push(secondStack.Pop());
+            }
+        }
+
+        public T Peek()
+        {
+            if (front == null)
+                throw new StackEmptyException();
+
+            return front.Value;
+        }
+
+        //if (firstStack.IsEmpty())
+        //        throw new QueueEmptyException();
+
+        public class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+                Next = null;
+            }
+
+            public Node Next { get; set; }
+            public T Value { get; set; }
+        }
+
     }
+
+    
 }
