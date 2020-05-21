911042e04516b7fb772345dd9087206daefb8993
cnickels21 <cnickels_21@yahoo.com>
2020-05-20 21:10:50 -0500

    enum property correctly added to db

--- Challenges.Tests/StacksAndQueues.Tests/MultiBracketValidationTests.cs
+++ Challenges.Tests/StacksAndQueues.Tests/MultiBracketValidationTests.cs
@@ -8,22 +8,6 @@ namespace Challenges.Tests.StacksAndQueues.Tests
     public class MultiBracketValidationTests
     {
         [Theory]
-        [InlineData("[")]
-        [InlineData("{")]
-        [InlineData("(")]
-        public void Can_push_brackets_into_stack(string input)
-        {
-            // Arrange
-            // Inline data
-
-            // Act
-            bool result = MultiBracketValidation.MixMatchTheSwitchCatch(input);
-
-            // Assert
-            Assert.True(result);
-        }
-
-        [Theory]
         [InlineData("[]")]
         [InlineData("{}")]
         [InlineData("()")]
@@ -40,6 +24,25 @@ namespace Challenges.Tests.StacksAndQueues.Tests
 
             // Assert
             Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("[")]
+        [InlineData("}")]
+        [InlineData("[)")]
+        [InlineData("[(])")]
+        [InlineData("{[()}")]
+        [InlineData("[{}")]
+        public void Can_return_false_for_all_combinations_of_invalid_brackets(string input)
+        {
+            // Arrange
+            // Inline data
+
+            // Act
+            bool result = MultiBracketValidation.MixMatchTheSwitchCatch(input);
+
+            // Assert
+            Assert.False(result);
         }
     }
 }
