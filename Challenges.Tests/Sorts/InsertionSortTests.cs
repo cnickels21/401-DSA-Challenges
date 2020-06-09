1e339b4ae9f45cbd598059fdd63785b82e63c795
cnickels21 <cnickels_21@yahoo.com>
2020-06-08 23:11:17 -0500

    all theory data passed

--- Challenges.Tests/Sorts/InsertionSortTests.cs
+++ Challenges.Tests/Sorts/InsertionSortTests.cs
@@ -23,6 +23,10 @@ namespace Challenges.Tests.Sorts
 
         [Theory]
         [InlineData(new[] { 3, 1, 2 }, new[] { 1, 2, 3 })]
+        [InlineData(new[] { 8, 4, 23, 42, 16, 15}, new[] { 4, 8, 15, 16, 23, 42 })]
+        [InlineData(new[] { 20, 18, 12, 8, 5, -2 }, new[] { -2, 5, 8, 12, 18, 20 })]
+        [InlineData(new[] { 5, 12, 7, 5, 5, 7 }, new[] { 5, 5, 5, 7, 7, 12 })]
+        [InlineData(new[] { 2, 3, 5, 7, 13, 11 }, new[] { 2, 3, 5, 7, 11, 13 })]
         public void Multiple_test_data_passed_through_sort(int[] testArray, int[] expected)
         {
             // Arrange in parameters
