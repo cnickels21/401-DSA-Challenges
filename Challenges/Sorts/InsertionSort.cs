af0809d8ae837c002ba2afab2c8e684cdcf985e1
cnickels21 <cnickels_21@yahoo.com>
2020-06-08 22:58:18 -0500

    insertion algorithm defined

--- Challenges/Sorts/InsertionSort.cs
+++ Challenges/Sorts/InsertionSort.cs
@@ -7,7 +7,22 @@ namespace Challenges.Sorts
     {
         public static int[] InsertionSorting(int[] array)
         {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i - 1;
+                int current = array[i];
+
+                while (j >= 0 && current < array[j])
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+
+                array[j + 1] = current;
+            }
+
             return array;
         }
+
     }
 }
