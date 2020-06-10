1e483301277432715e9108f04baef3062faaf13b
cnickels21 <cnickels_21@yahoo.com>
2020-06-09 20:36:44 -0500

    first theory data failed

--- Challenges.Tests/Sorts/MergeSortTests.cs
+++ Challenges.Tests/Sorts/MergeSortTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using Xunit;
 using Challenges.Sorts;
+using Xunit.Sdk;
 
 namespace Challenges.Tests.Sorts
 {
@@ -20,5 +21,19 @@ namespace Challenges.Tests.Sorts
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData(new int[] { 8, 4, 23, 42, 16, 15 }, new int[] { 4, 8, 15, 16, 23, 42 })]
+        public void Theory_to_merge_sort_variety_of_arrays(int[] actual, int[] expected)
+        {
+            // Arrange in parameters
+
+            // Act
+            int[] result = MergeSort.MergeSorter(actual);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
     }
 }
