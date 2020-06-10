bd436a4dfff355401ff220192a887380c3884a83
cnickels21 <cnickels_21@yahoo.com>
2020-06-10 00:03:17 -0500

    to do comment in wrong place

--- Challenges.Tests/Sorts/MergeSortTests.cs
+++ Challenges.Tests/Sorts/MergeSortTests.cs
@@ -26,9 +26,9 @@ namespace Challenges.Tests.Sorts
         // ToDo: Fix divide by odd length array
         // [InlineData(new int[] { 8, 4, 23, 42, 16, 15 }, new int[] { 4, 8, 15, 16, 23, 42 })]
         [InlineData(new int[] { 3, 2 }, new int[] { 2, 3 })]
+        [InlineData(new int[] { 3, 2, 4, 1 }, new int[] { 1, 2, 3, 4 })]
         // ToDo: Fix divide by odd length array
-        // [InlineData(new int[] { 3, 2, 4, 1 }, new int[] { 1, 2, 3, 4 })]
-        [InlineData(new int[] { 3, 2, 5, 4, 1 }, new int[] { 1, 2, 3, 4, 5 })]
+        // [InlineData(new int[] { 3, 2, 5, 4, 1 }, new int[] { 1, 2, 3, 4, 5 })]
         public void Theory_to_merge_sort_variety_of_arrays(int[] actual, int[] expected)
         {
             // Arrange in parameters
