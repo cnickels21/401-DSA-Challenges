3b5b936fa6d0a377398ab675e1d2afa440e8c53b
cnickels21 <cnickels_21@yahoo.com>
2020-06-14 17:47:51 -0500

    can run string through get hash function

--- DataStructures.Tests/HashTable.Tests/HashTableTests.cs
+++ DataStructures.Tests/HashTable.Tests/HashTableTests.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Text;
+﻿
 using DataStructures.HashTable;
+using System;
+using System.Text;
 using Xunit;
 
 namespace DataStructures.Tests.HashTable.Tests
 {
     public class HashTableTests
     {
+        [Fact]
+        public void Can_hash_string_into_key()
+        {
+            // Arrange
+            string key = "Cat";
+
+            // Act
+            int result = DataStructures.HashTable.HashTable.GenerateHashCode(key);
+
+            // Assert
+            Assert.Equal(14, result);
+        }
     }
 }
--- DataStructures/HashTable/HashTable.cs
+++ DataStructures/HashTable/HashTable.cs
@@ -5,5 +5,24 @@ namespace DataStructures.HashTable
 {
     public class HashTable
     {
+        public static int GenerateHashCode(string key)
+        {
+            if (key is null || key == "")
+                throw new ArgumentException("Cannot hash key");
+
+            int asciiSum = 0;
+
+            foreach (char letter in key)
+            {
+                int letterValue = (int)letter;
+                asciiSum += letterValue;
+            }
+
+            int hashProduct = asciiSum * 599;
+
+            int hashIndex = hashProduct % 99;
+
+            return hashIndex;
+        }
     }
 }
