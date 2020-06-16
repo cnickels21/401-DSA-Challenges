b17e070fb810ba33807c563aeb339f41cf986a32
cnickels21 <cnickels_21@yahoo.com>
2020-06-16 16:55:41 -0500

    tests for returning empty list if either or both trees are empty

--- Challenges.Tests/DuplicateSearch.Tests/FindMatchesTests.cs
+++ Challenges.Tests/DuplicateSearch.Tests/FindMatchesTests.cs
@@ -1,10 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DataStructures.BinaryTree;
+using System;
 using System.Text;
+using Xunit;
+using Challenges.HashTableStuff;
+using DataStructures;
 
 namespace Challenges.Tests.DuplicateSearch.Tests
 {
-    class FindMatchesTests
+    public class FindMatchesTests
     {
+        [Fact]
+        public void Empty_tree_returns_empty_list()
+        {
+            // Arrange
+            BinarySearchTree<int> treeOne = new BinarySearchTree<int>();
+            BinarySearchTree<int> treeTwo = new BinarySearchTree<int>();
+            LinkedList expected = new LinkedList();
+
+            // Act
+            LinkedList result = FindMatches.TreeIntersection(treeOne, treeTwo);
+
+            // Assert
+            Assert.True(result.Head == null);
+        }
+
+        [Fact]
+        public void One_empty_tree_returns_empty_list()
+        {
+            // Arrange
+            BinarySearchTree<int> treeOne = new BinarySearchTree<int>();
+            BinarySearchTree<int> treeTwo = new BinarySearchTree<int>();
+            LinkedList expected = new LinkedList();
+
+            treeOne.Add(1);
+
+            // Act
+            LinkedList result = FindMatches.TreeIntersection(treeOne, treeTwo);
+
+            // Assert
+            Assert.True(result.Head == null);
+        }
+
+        [Fact]
+        public void Second_empty_tree_returns_empty_list()
+        {
+            // Arrange
+            BinarySearchTree<int> treeOne = new BinarySearchTree<int>();
+            BinarySearchTree<int> treeTwo = new BinarySearchTree<int>();
+            LinkedList expected = new LinkedList();
+
+            treeTwo.Add(1);
+
+            // Act
+            LinkedList result = FindMatches.TreeIntersection(treeOne, treeTwo);
+
+            // Assert
+            Assert.True(result.Head == null);
+        }
     }
 }
--- Challenges/HashTableStuff/FindMatches.cs
+++ Challenges/HashTableStuff/FindMatches.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DataStructures;
+using DataStructures.BinaryTree;
+using System;
+using System.Collections;
 using System.Text;
 
 namespace Challenges.HashTableStuff
 {
-    class FindMatches
+    public class FindMatches
     {
+        public static LinkedList TreeIntersection(BinaryTree<int> treeOne, BinaryTree<int> treeTwo)
+        {
+            LinkedList result = new LinkedList();
+
+            if (treeOne.Root == null || treeTwo.Root == null)
+                return result;
+
+            return result;
+        }
     }
 }
