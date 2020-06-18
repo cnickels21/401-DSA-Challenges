fef7cfe5e788e5c116606abf09f6b997833a8004
cnickels21 <cnickels_21@yahoo.com>
2020-06-17 21:29:51 -0500

    empty left join class and test defined

---
+++ Challenges.Tests/DuplicateSearch.Tests/LeftJoinTablesTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DataStructures.HashTable;
+
+namespace Challenges.Tests.DuplicateSearch.Tests
+{
+    public class LeftJoinTablesTests
+    {
+    }
+}
---
+++ Challenges/HashTableStuff/LeftJoinTables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures.HashTable;
+
+namespace Challenges.HashTableStuff
+{
+    public class LeftJoinTables
+    {
+    }
+}
