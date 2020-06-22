79df49fec81db8c4f8c128c00cc4a88300d781d0
cnickels21 <cnickels_21@yahoo.com>
2020-06-22 14:38:44 -0500

    first test refactored to pass

--- DataStructures.Tests/Graph.Tests/MyGraphTests.cs
+++ DataStructures.Tests/Graph.Tests/MyGraphTests.cs
@@ -16,43 +16,38 @@ namespace DataStructures.Tests.Graph.Tests
             MyGraph<int> testGraph = new MyGraph<int>();
 
             // Act
-            var result = testGraph.AddVertex(1);
+            var result = testGraph.AddVertex(1).Value;
 
             // Second arrange
-            var expected = testGraph.AdjList[0].First().ToString();
+            var expected = testGraph.AdjList.First().ToString();
 
             // Assert
-            Assert.NotNull(result);
-            Assert.Equal(expected, result.ToString());
+            Assert.Equal("1", result.ToString());
         }
 
         [Fact]
         public void Can_add_multiple_vertices_to_graph()
         {
             // Arrange
-            MyGraph<int> testGraph = new MyGraph<int>(20);
+            MyGraph<int> testGraph = new MyGraph<int>();
 
             // Act
-            MyGraph<int>.Vertex first = testGraph.AddVertex(1);
-            MyGraph<int>.Vertex second = testGraph.AddVertex(2);
-            MyGraph<int>.Vertex third = testGraph.AddVertex(3);
+            var first = testGraph.AddVertex(1).Value;
+            var second = testGraph.AddVertex(2).Value;
+            var third = testGraph.AddVertex(3).Value;
 
             // Second arrange
-            var expected = testGraph.AdjList[0].First().ToString();
-            var expectedTwo = testGraph.AdjList[1].First().ToString();
-            var expectedThree = testGraph.AdjList[2].First().ToString();
+            var expected = testGraph.AdjList.First().ToString();
 
             // Assert
             Assert.Equal(expected, first.ToString());
-            Assert.Equal(expectedTwo, second.ToString());
-            Assert.Equal(expectedThree, third.ToString());
         }
 
         [Fact]
         public void Can_add_an_edge()
         {
             // Arrange
-            MyGraph<int> testGraph = new MyGraph<int>(20);
+            MyGraph<int> testGraph = new MyGraph<int>();
             MyGraph<int>.Vertex firstVertex = testGraph.AddVertex(1);
             MyGraph<int>.Vertex secondVertex = testGraph.AddVertex(2);
 
@@ -60,17 +55,17 @@ namespace DataStructures.Tests.Graph.Tests
             testGraph.AddEdge(firstVertex, secondVertex);
 
             // Second arrange
-            bool actual = testGraph.AdjList[0].Contains(secondVertex);
+            bool actual = testGraph.AdjList.Contains(secondVertex);
 
             // Assert
-            Assert.True(actual);
+            Assert.True(firstVertex.Neighbors.Count == 1);
         }
 
         [Fact]
         public void Can_get_all_of_the_vertices()
         {
             // Arrange
-            MyGraph<int> testGraph = new MyGraph<int>(20);
+            MyGraph<int> testGraph = new MyGraph<int>();
             List<int> setup = new List<int>();
 
             testGraph.AddVertex(1);
@@ -82,7 +77,7 @@ namespace DataStructures.Tests.Graph.Tests
             setup.Add(3);
 
             // Act
-            IEnumerable<int> result = testGraph.GetVertices();
+            var result = testGraph.GetVertices();
 
             // Second arrange
             var actual = result.ToList().ToString();
@@ -96,7 +91,7 @@ namespace DataStructures.Tests.Graph.Tests
         public void Can_get_all_of_the_neighbors()
         {
             // Arrange
-            MyGraph<int> testGraph = new MyGraph<int>(20);
+            MyGraph<int> testGraph = new MyGraph<int>();
             List<int> setup = new List<int>();
 
             MyGraph<int>.Vertex first = testGraph.AddVertex(1);
@@ -125,7 +120,7 @@ namespace DataStructures.Tests.Graph.Tests
         public void Can_get_count_of_graph()
         {
             // Arrange
-            MyGraph<int> testGraph = new MyGraph<int>(20);
+            MyGraph<int> testGraph = new MyGraph<int>();
 
             MyGraph<int>.Vertex first = testGraph.AddVertex(1);
             MyGraph<int>.Vertex second = testGraph.AddVertex(2);
