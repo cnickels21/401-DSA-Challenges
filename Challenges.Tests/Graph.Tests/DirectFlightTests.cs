3dd28744feff3e387bcca0214914f2884202c841
cnickels21 <cnickels_21@yahoo.com>
2020-06-23 17:18:29 -0500

    empty direct flight class and test defined

---
+++ Challenges.Tests/Graph.Tests/DirectFlightTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures.Graph;
+using Xunit;
+
+namespace Challenges.Tests.Graph.Tests
+{
+    public class DirectFlightTests
+    {
+    }
+}
---
+++ Challenges/Graph/DirectFlight.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures.Graph;
+
+namespace Challenges.Graph
+{
+    public class DirectFlight
+    {
+    }
+}
